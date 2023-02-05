ec030ca5d6c0e90521fdb8b0cde50c148377667d
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-05 22:14:17 +0600

    Update for-loop1.js

---
+++ For-Loop/for-loop1.js
@@ -0,0 +1,13 @@
+//For Loop -Odd Number - Sum
+let p= 10;
+let Sum =0 ;
+
+for(let i=1; i<=p; i++){
+  if(i%2==1){
+    Sum =Sum+ i;
+    console.log('odd Number: ' + i +' . & Sum is: '+ Sum);
+
+  };
+ 
+};
+ console.log('Final Sum: '+ Sum);
