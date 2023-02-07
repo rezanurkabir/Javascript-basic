4082a6a00e9bfcfad367d8aa5c88c41d851e8e56
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-07 21:26:08 +0600

    Update for-loop1.js

--- For-Loop/for-loop1.js
+++ For-Loop/for-loop1.js
@@ -11,3 +11,18 @@ for(let i=1; i<=p; i++){
  
 };
  console.log('Final Sum: '+ Sum);
+
+//For Loop -Even Number - Sum *****************
+
+let n= 10;
+let Sum =0 ;
+
+for(let i=1; i<=n; i++){
+  if(i%2==0){
+    Sum =Sum+ i;
+    console.log('Even Number: ' + i +' . & Sum is: '+ Sum);
+
+  };
+ 
+};
+ console.log('Final Sum: '+ Sum)
