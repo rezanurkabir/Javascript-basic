c64f5f36f12f4bc3e8135a392deab286ebb104fb
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-11 20:49:18 +0600

    Add files via upload

---
+++ Array/Array.js
@@ -0,0 +1,9 @@
+//Simple Array:
+let myArray = ['Bangladesh',100,'Reza',['Dhaka','Khulna']]
+
+//Array Access
+ console.log(myArray[0])
+
+//Array Constructor 
+let arrConst =new Array ('Rahim','Reza',100)
+console.log(arrConst)
