a8779a8dd9edc61afa1655f01cfa2a774b54ef9f
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-21 20:57:55 +0600

    Update problem-solve.js

--- problem-solve/problem-solve.js
+++ problem-solve/problem-solve.js
@@ -9,3 +9,22 @@ function randomNumber (min,max){
  let output = randomNumber (1,10)
 
  console.log(output)
+
+
+// Function 
+
+var arr1 = [1,3,6]
+var arr2 = [2,3,4]
+function arrSum(arr){
+
+ var sum = 0 ;
+  
+ for(let i = 0 ; i< arr.length ; i++){
+   sum = sum +arr[i]
+ }
+  return sum;
+}
+
+
+var sumArr = arrSum
+console.log(sumArr(arr2))
