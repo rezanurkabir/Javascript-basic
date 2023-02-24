278f2ff8ba62b104ba1e0b1d7a8753ea06a40a0f
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-24 22:57:03 +0600

    Update Array.js

--- Array/Array.js
+++ Array/Array.js
@@ -7,3 +7,12 @@ let myArray = ['Bangladesh',100,'Reza',['Dhaka','Khulna']]
 //Array Constructor 
 let arrConst =new Array ('Rahim','Reza',100)
 console.log(arrConst)
+
+
+// Square 
+var arr = [2,5,6]
+var newArr = []
+for(let i=0 ; i<arr.length ; i++){
+  newArr.push (arr[i] * arr[i] );
+}
+console.log(newArr);
