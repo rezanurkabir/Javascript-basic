ff92b5d08171e064125f7d3fc0f85956db50215c
Rezanur Kabir Pintu <59076895+rezanurkabir@users.noreply.github.com>
2023-02-25 23:04:45 +0600

    Update Array.js

--- Array/Array.js
+++ Array/Array.js
@@ -16,3 +16,11 @@ for(let i=0 ; i<arr.length ; i++){
   newArr.push (arr[i] * arr[i] );
 }
 console.log(newArr);
+
+// Filter Method
+var arr1 = [1,2,3,4,5,6]
+function getEven(item){
+  return item % 2 == 0;
+}
+var arr2 = arr1.filter(getEven)
+console.log(arr2)
